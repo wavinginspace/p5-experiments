26da95f634da6da5b405555461562c65544bd9dd
wavinginspace <paulbaisley@gmail.com>
2021-04-18 22:59:25 -0400

    perlin static generator

---
+++ nature-of-code/1.2/script.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+// x offset for perlin noise. different on every page refresh, but stays the same after load.
+// const xoff1 = 0;
+// const xoff2 = 10000;
+
+// increment by
+let inc = 0;
+// x start point for perlin noise
+// let start = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  pixelDensity(1);
+  frameRate(30);
+  // background(100);
+}
+function draw() {
+  let yoff = inc;
+  loadPixels();
+  noiseDetail(mouseX / 100, 0.1);
+  for (let y = 0; y < height; y++) {
+    let xoff = inc;
+    for (let x = 0; x < width; x++) {
+      let r = noise(xoff, yoff) * 255;
+      let index = (x + y * width) * 4;
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = random(240, 255);
+      xoff += sin(mouseY);
+    }
+    yoff += mouseX;
+  }
+  // xoff += inc;
+  updatePixels();
+
+  // if (mouseIsPressed) {
+  //   ellipse(mouseX, mouseY, 50, 50);
+  // }
+
+  inc += mouseX;
+  // noLoop();
+  // const x = map(noise(xoff1), 0, 1, 0, width);
+  // const y = map(noise(xoff2), 0, 1, 0, height);
+
+  // // keep this number small to maintain smoothness of perlin noise.
+  // // if too high, will jump around a lot and be more like random
+  // xoff1 += 0.002;
+  // xoff2 += 0.005;
+
+  // ellipse(x, y, 24, 24);
+}
